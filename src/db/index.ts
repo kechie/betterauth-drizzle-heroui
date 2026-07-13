97bf6a95dc83ad74f198d3f921b427a882a8e21f
Richard Alhama <richard.alhama@gmail.com>
2026-07-13 16:45:04 +0800

    refactor export

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,6 @@
-//import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-// You can specify any property from the node-postgres connection options
-const db = drizzle({
+export const db = drizzle({
   connection: {
     connectionString: process.env.DATABASE_URL!,
     ssl: false
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,15 @@
-/* import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "@better-auth/drizzle-adapter";
-import { db } from "@db/index.ts";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle-adapter";
+import { db } from "../db";
+
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg", // or "pg" or "mysql"
-  }),
-  //secret: "",
+    database: drizzleAdapter(db, {
+        provider: "pg",
+    }),
+    emailAndPassword: {
+        enabled: true // Required if you are building standard sign-in/sign-up components
+    }
 });
- */
 
- import { betterAuth } from "better-auth";
- import { drizzleAdapter } from "better-auth/adapters/drizzle";
- import { db } from "../db"; // your drizzle instance
 
- export const auth = betterAuth({
-     database: drizzleAdapter(db, {
-         provider: "pg", // or "mysql", "sqlite"
-     }),
- });
+// TODO: disable signup after the initial sign-up
